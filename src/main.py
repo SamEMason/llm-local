f9d6a933f39c0c57861d937357834b04eea901ea
sam <me@sammason.dev>
2025-06-19 15:33:59 -0400

    add route and config changes to main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
-import os
+from src.config import load_config
+from src.routes import router
 
+
+config = load_config()
 app = FastAPI()
+
+app.include_router(router=router, prefix=config["api"]["endpoint"])
 
 
 @app.get("/")
 async def test_endpoint():
-    env = os.getenv("ENV", "undefined")
+    env = config.get("env", "undefined")
     return {"message": f"Running in {env} mode"}
